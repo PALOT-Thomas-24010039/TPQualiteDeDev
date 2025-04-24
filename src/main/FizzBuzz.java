bb3a98bce129d8cf634626dcef72db52280d904e
ThomasPalot <thomas.palot@etu.univ-amu.fr>
2025-04-24 14:31:02 +0200

    implementation de la methode getResult et ajout des test pour chaque cas possible

--- src/main/FizzBuzz.java
+++ src/main/FizzBuzz.java
@@ -3,6 +3,19 @@ package main;
 public class FizzBuzz {
 
     public String getResult(int i) {
-        return "1";
+
+        if (i % 3 == 0 && i % 5 == 0) {
+            return "FizzBuzz";
+        }
+        else if (i % 3 == 0) {
+            return "Fizz";
+        }
+        else if (i % 5 == 0) {
+            return "Buzz";
+        }
+        else {
+            return Integer.toString(i);
+        }
+
     }
 }
--- src/test/testFizzBuzz.java
+++ src/test/testFizzBuzz.java
@@ -4,14 +4,33 @@ import main.FizzBuzz;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
+
 
 public class testFizzBuzz {
 
     @Test
-    public void TestFizzBuzz(){
-        //fail("");
+    public void testFizz(){
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        assertEquals("Fizz", fizzBuzz.getResult(3));
+    }
+
+    @Test
+    public void testBuzz(){
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        assertEquals("Buzz", fizzBuzz.getResult(5));
+    }
+
+    @Test
+    public void testFizzBuzz15(){
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        assertEquals("FizzBuzz", fizzBuzz.getResult(15));
+    }
+
+    @Test
+    public void testBuzzBuzz1(){
         FizzBuzz fizzBuzz = new FizzBuzz();
         assertEquals("1", fizzBuzz.getResult(1));
     }
+
+
 }
